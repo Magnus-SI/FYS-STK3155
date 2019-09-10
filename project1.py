ef942bc260017353fc01380ffe9f0dd22debdd5a
Magnus-SI <40592328+Magnus-SI@users.noreply.github.com>
2019-09-10 10:04:05 +0200

    now has OLS

--- project1.py
+++ project1.py
@@ -6,11 +6,13 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import matplotlib.tri as mtri
+from numpy.polynomial.polynomial import polyvander2d
 
 class idk:
     def __init__(self, seed=1):
         np.random.seed(seed)
         self.data=False
+        self.hasfit=False
         pass
 
     def FrankeFunction(self,x,y):
@@ -25,20 +27,42 @@ class idk:
         y_exact = self.FrankeFunction(x1,x2)
         sigma = np.max(y_exact)*noisefraq
         mu = 0
-        y = y_exact + np.random.normal(sigma,mu,size=N)
+        y = y_exact + np.random.normal(mu, sigma, size=N)
         self.x1, self.x2, self.y_exact, self.y = x1, x2, y_exact, y
         self.N = N
         self.data=True
 
-    def plot3D(self,approx=False):
+    def fit(self, method, deg=(2,2)):
+        X=polyvander2d(self.x1,self.x2,deg)
+        beta=method(X)
+        y_pred=X@beta
+        self.hasfit=True
+        self.y_pred, self.beta = y_pred, beta
+
+    def OLS(self,X):
+        #return np.linalg.inv(np.transpose(X)@X)@X@self.y
+        XtXinv=np.linalg.inv(np.einsum('ij,ik',X,X))
+        return np.einsum('ij,kj,k',XtXinv,X,self.y)
+
+    def plot3D(self,usenoisy=True,approx=False):
+        if usenoisy:
+            y=self.y
+        else:
+            y=self.y_exact
         if not self.data:
             print("Generate data first")
             return
         triang=mtri.Triangulation(self.x1,self.x2)
         fig=plt.figure()
         ax=fig.add_subplot(1,1,1, projection='3d')
-        ax.plot_trisurf(triang,self.y,cmap='jet')
-        ax.scatter(self.x1, self.x2, self.y, marker='.', s=10, c='black', alpha=0.5)
+        #ax.plot_trisurf(triang,self.y,cmap='jet')
+        ax.scatter(self.x1, self.x2, y, marker='.', s=10, c='green', alpha=0.5)
+        if approx:
+            if self.hasfit:
+                ax.scatter(self.x1, self.x2, self.y_pred, marker='.', s=10, c='black', alpha=0.5)
+            else:
+                print("Fit data first")
+
         ax.view_init(elev=60, azim=-45)
 
         """
@@ -52,3 +76,9 @@ class idk:
 
         plt.show()
         pass
+
+if __name__=="__main__":
+    I=idk()
+    I.gendat(300,noisefraq=0.001)
+    I.fit(I.OLS,(5,5))
+    I.plot3D(True, True)
