48b28c33e2570ce80dd648669d00b74eeaf57da1
fredln <fredriln@uio.no>
2019-09-10 09:34:54 +0200

    update proj11.py

--- proj11.py
+++ proj11.py
@@ -10,20 +10,25 @@ from numpy.polynomial.polynomial import polyvander2d
 fig=plt.figure()
 ax=fig.gca(projection='3d')
 
-"Make data for plot:"
-x = np.arange(0, 1, 0.05)
-y = np.arange(0, 1, 0.05)
-x,y = np.meshgrid(x,y)
 
-def FrankeFunction(x,y):
-    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2)-0.25*((9*y-2)**2))
-    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
-    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
-    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+"Make data for plot:"
+x1 = np.arange(0, 1, 0.05)
+x2 = np.arange(0, 1, 0.05)
+deg = 5 #Degree of the approximation
+X = polyvander2d(x1,x2,deg)
+x,y = np.meshgrid(x1,x2)
+
+def FrankeFunction(x1,x2):
+    term1 = 0.75*np.exp(-(0.25*(9*x1-2)**2)-0.25*((9*x2-2)**2))
+    term2 = 0.75*np.exp(-((9*x1+1)**2)/49.0 - 0.1*(9*x2+1))
+    term3 = 0.5*np.exp(-(9*x1-7)**2/4.0 - 0.25*((9*x2-3)**2))
+    term4 = -0.2*np.exp(-(9*x1-4)**2 - (9*x2-7)**2)
     return term1 + term2 + term3 + term4
 
-z=FrankeFunction(x,y)
+def OLS(X,y):
+    return np.linalg.inv(np.transpose(X)@X)@X@y
 
+z=FrankeFunction(x,y)
 "Plot surface:"
 surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
 ax.set_zlim(-0.1, 1.4)
@@ -44,3 +49,4 @@ sigma=0.02*np.max(z)
 mu=0
 noise=np.random.normal(mu,sigma,size=N)
 z+=noise
+betas = OLS(X,z)
