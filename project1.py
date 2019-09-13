22d0829517add7c7c740e3d34d43eb1bdb69231c
Magnus-SI <40592328+Magnus-SI@users.noreply.github.com>
2019-09-13 18:19:11 +0200

    Added descriptions of the various functions

--- project1.py
+++ project1.py
@@ -11,11 +11,12 @@ from numpy.polynomial.polynomial import polyvander2d
 class idk:
     def __init__(self, seed=2):
         np.random.seed(seed)
-        self.data=False
-        self.hasfit=False
+        self.data=False         #data is not yet generated
+        self.hasfit=False       #a fit has not yet been made
         pass
 
     def FrankeFunction(self,x,y):
+        #The franke function as given
         term1 = 0.75*np.exp(-(0.25*(9*x-2)**2)-0.25*((9*y-2)**2))
         term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
         term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
@@ -23,33 +24,58 @@ class idk:
         return term1 + term2 + term3 + term4
 
     def gendat(self,N,noisefraq=0.05, Function=FrankeFunction):
+        """
+        The data generation could, and probably should be changed to generate
+        linspaced data as to allow for easier plotting, but also easier fitting.
+        However, the code should be general enough that it would work anyways.
+
+        N: Amount of data points generated
+        noisefraq: fraction of data range in the y-direction as standard deviation
+        """
         x1,x2 = np.random.uniform(0,1,size=(2,N))
         y_exact = self.FrankeFunction(x1,x2)
-        sigma = np.max(y_exact)*noisefraq
+        sigma = (np.max(y_exact)-np.min(y_exact))*noisefraq
         mu = 0
         y = y_exact + np.random.normal(mu, sigma, size=N)
         self.x1, self.x2, self.y_exact, self.y = x1, x2, y_exact, y
         self.N = N
-        self.data=True
+        self.data=True      #checks that data is generated
 
     def fit(self, method, deg=(2,2)):
+        """
+        method: method of fit. Would eventually be a choice between OLS, Ridge, Lasso
+        deg: degree of polynomial to fit to
+        (lambda): necessary when Ridge and Lasso is implemented
+        """
         X=polyvander2d(self.x1,self.x2,deg)
         beta=method(X)
         y_pred=X@beta
-        self.hasfit=True
+        self.hasfit=True        #a fit has now been made
         self.y_pred, self.beta = y_pred, beta
 
     def OLS(self,X):
-        #return np.linalg.inv(np.transpose(X)@X)@X@self.y
+        """
+        OLS using our given formulas, note that this as of now does not work with
+        polynomials of too high degree.
+        """
         XtXinv=np.linalg.inv(np.einsum('ij,ik',X,X))
         return np.einsum('ij,kj,k',XtXinv,X,self.y)
 
     def OLS2(self,X):
+        #OLS using scikit-learn
         lr=LinearRegression(fit_intercept=False)
         lr.fit(X,self.y)
         return lr.coef_
 
     def Error(self,usenoisy=True):
+        """
+        Input:
+        usenoisy: whether to compare the error of the predicted y
+        to the noisy y or the actual y
+
+        Returns the MSE, and later the R2 score
+        """
+
         if usenoisy:
             y=self.y
 
@@ -59,6 +85,12 @@ class idk:
         return MSE
 
     def ErrorAnalysis(self, poldeg=(5,5), noises=np.logspace(-2,2,5)):
+        """
+        Some basic example of error analysis that compares the MSE error as
+        it changes due to noise, and how this is different when comparing
+        to noisy, or not noisy data.
+        """
+
         noises=np.array(noises)
         fig=plt.figure()
         ax=fig.add_subplot(1,1,1)
@@ -82,6 +114,14 @@ class idk:
 
 
     def plot3D(self,usenoisy=True,approx=False):
+        """
+        usenoisy: whether to plot the noisy data, or the actual data
+        approx: whether to plot the approximated fit of the data
+
+        Note that this method works in general, but if we end up using linspaced
+        data in a meshgrid, a normal contourplot would be better
+        """
+
         if usenoisy:
             y=self.y
         else:
@@ -89,7 +129,7 @@ class idk:
         if not self.data:
             print("Generate data first")
             return
-        triang=mtri.Triangulation(self.x1,self.x2)
+        triang=mtri.Triangulation(self.x1,self.x2)          #nevessary for unevenly spaced data
         fig=plt.figure()
         ax=fig.add_subplot(1,1,1, projection='3d')
         #ax.plot_trisurf(triang,self.y,cmap='jet')
