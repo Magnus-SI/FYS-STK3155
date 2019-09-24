49b9322475033df3692897bd3abc8b5b1a4e085c
fredln <fredriln@student.matnat.uio.no>
2019-09-24 10:11:18 +0200

    Update terrain.py

--- terrain.py
+++ terrain.py
@@ -1,19 +1,30 @@
-from project1 import idk, OLS
+from project1 import idk, OLS, MSE, OLS2
 from imageio import imread
 import pandas as pd
 import numpy as np
-from Ridge import Ridge
+from Ridge import Ridge, Ridgeskl
+from Lasso import Lasso
+import matplotlib.pyplot as plt
+
+def MSE(data,model):
+    N = data.size
+    return np.sum((data-model)**2)/N
+
+def OLS3(X,y):
+    beta = np.linalg.pinv(X)@y
+    return beta
 
 class Terrain(idk):
-    def set_data(self,data,deg = (5,5)):
+    def set_data(self,data,deg = (10,10)):
         """
         Set the data
         """
         self.polydeg = deg
         df = pd.DataFrame()
-        self.N = data.shape[0]*data.shape[1]
+        self.N = data.size
+        self.Nx, self.Ny = data.shape
 
-        x1, x2 = np.meshgrid(np.arange(data.shape[0]),np.arange(data.shape[1]))
+        x1, x2 = np.meshgrid(np.arange(self.Ny),np.arange(self.Nx))
         x1 = x1.flatten()
         x2 = x2.flatten()
         y = data.flatten()
@@ -24,16 +35,60 @@ class Terrain(idk):
         self.df = df
         self.changepolydeg(polydeg = deg)
 
+    def plot_terrain(self):
+        """
+        plots the terrain data in a 2D plot
+        needs no input
+        """
+        plt.figure()
+        plt.title('Terrain over Norway 1')
+        plt.imshow(self.df['y'].values.reshape(self.Nx,self.Ny), cmap='gray')
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.show()
+
+    def plot_fit(self):
+        """
+        plots the fit, need no input, but the method fit has to be run before
+        """
+        plt.figure()
+        plt.title('Fit')
+        print((self.X@self.beta).shape)
+        plt.imshow((self.X@self.beta).reshape(self.Nx,self.Ny), cmap='gray')
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.show()
+
+    def fit_frac(self,method,frac):
+        """
+        fits a random sample of the dataset
+        takes parameter frac : the fraction of the dataset that should be fitted
+        """
+        df = self.df.sample(frac=frac)
+        self.fit(method,df = df)
+
+
 if __name__ == '__main__':
     filename = "SRTM_data_Norway_1.tif"
     terrain_data = imread(filename)
-
-    _lambda = 1e-2
-    R = Ridge(_lambda)
     terrain = Terrain()
     terrain.set_data(terrain_data)
-    terrain.fit(OLS)
-    print("betas (OLS) = ",terrain.beta)
+
+    terrain.plot_terrain()
+
+    _lambda = 0.001
+    frac = 0.0001
+    R = Ridgeskl(_lambda)
+    terrain.fit_frac(R,frac)
+    print("betas = ",terrain.beta)
+    print("MSE = ",MSE(terrain.df['y'],terrain.X@terrain.beta))
+
+    terrain.plot_fit()
+    """
+    terrain.fit(OLS3)
+
 
     terrain.fit(R)
     print("betas (Ridge, lambda = %g)"%(_lambda), terrain.beta)
+    print("betas (Ridge, lambda = %g)"%(_lambda), MSE(terrain.df['y'],terrain.X@terrain.beta))
+    """
