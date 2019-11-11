c01180a03d4c3c6e3920053fda2902f6982930ea
Magnus-SI <40592328+Magnus-SI@users.noreply.github.com>
2019-11-11 02:50:03 +0100

    a lot of analysis

--- Project2/NNregressor.py
+++ Project2/NNregressor.py
@@ -84,7 +84,7 @@ class FrankeNN(Project1):
         plt.ylabel(self.cost)
         plt.show()
 
-    def multiepoch(self, polydeg, noise, epoch_arr):
+    def multiepoch(self, polydeg, noise, epoch_arr, newplot = True):
         deg = int(polydeg)
         self.changenoise(noisefraq=noise)
         self.changepolydeg(polydeg = (deg, deg))
@@ -95,14 +95,54 @@ class FrankeNN(Project1):
         for i,epoch in enumerate(epoch_arr):
             self.epochs = int(epoch)
             #errs[i] = self.kfolderr(method = self.FFNN.fit)
-            errs[i] = self.traintesterr()
-        plt.figure()
+            errs[i] = self.traintesterr(testerr = False)
+        if newplot:
+            plt.figure()
         plt.title(r"$log10(\sigma)$=%.1f, polynomial order = $%i$"%(np.log10(noise), deg))
-        plt.plot(epoch_arr, errs)
+        plt.plot(epoch_arr, errs, label = r"$\eta$ = %.3f"%self.eta)
         plt.xlabel("epoch")
         plt.ylabel("%s"%self.cost)
         if self.cost == "MSE":
-            self.yscale("log")
+            plt.yscale("log")
+        plt.legend()
+        plt.show()
+
+    def biasvar(self, K, param_arr, param_name):
+        split = int(0.2*self.N)             #80-20 train-test split
+        MSEs = np.zeros(len(param_arr))
+        biass = np.zeros(len(param_arr))
+        variances = np.zeros(len(param_arr))
+        dftrain, dftest = np.split(self.df, [split])    #performs the split
+        testinds = dftest.index
+        y = dftest['y'].values
+        msebest = 1e10                      #temporary worst value, for saving the best value
+        self.FFNN.doreset = True
+        if not self.compnoisy:
+            y = dftest['y_exact'].values
+        for j,paramval in enumerate(param_arr):
+            self.parachanger(param_name, paramval)
+            ypreds = np.zeros((len(dftest), K))
+            for i in range(K):
+                #df = dftrain.sample(frac=1.0, replace=True)
+                df = dftest
+                self.fit(self.FFNN.fit, df)
+                ypreds[:,i] = self.FFNN.predict(self.X[testinds])[0]
+            MSEs[j] = np.mean(np.mean((y-ypreds.transpose())**2, axis=0))
+            biass[j] = np.mean((y-np.mean(ypreds,axis=1))**2)
+            variances[j] = np.mean(np.var(ypreds, axis=1))
+
+        plt.figure()
+        if param_name== "hlayer":
+            param_arr = np.arange(1, len(param_arr)+1)
+        plt.plot(param_arr, MSEs, label="MSE")
+        plt.plot(param_arr, biass, label="bias")
+        plt.plot(param_arr, variances, label="variance")
+        plt.plot(param_arr, biass+variances,'--', label="bias+var")
+        plt.legend()
+        if self.cost=="MSE":
+            plt.yscale("log")
+        plt.xlabel(param_name)
+        plt.ylabel(self.cost)
         plt.show()
 
 
@@ -111,10 +151,8 @@ class FrankeNN(Project1):
         TrainErrors = np.zeros((len(polydegs), len(noises)))
         showvals = True
         self.FFNN.doreset = True
-        self.epochs = 1000
         for i,deg in enumerate(polydegs):
-            self.changepolydeg((deg,deg))
-            self.initNN()
+            self.parachanger("deg", deg)
 
             for j,noise in enumerate(noises):
                 self.changenoise(noisefraq=noise)
@@ -122,17 +160,19 @@ class FrankeNN(Project1):
 
                 #TestErrors[i,j] = self.kfolderr(ks = np.arange(2,6), method = FNN.FFNN.fit)
                 TestErrors[i,j] = self.traintesterr()
-                TrainErrors[i,j] = self.traintesterr(testerr = False)#self.fiterr(method = FNN.FFNN.fit)
-        f, axs = plt.subplots(2,1, figsize=(12,12))
-        ax1, ax2 = axs
+                #TrainErrors[i,j] = self.traintesterr(testerr = False)#self.fiterr(method = FNN.FFNN.fit)
+        #f, axs = plt.subplots(2,1, figsize=(12,12))
+        #ax1, ax2 = axs
+        f, ax1 = plt.subplots(1,1, figsize = (12,12))
         h1=sns.heatmap(data=TestErrors,annot=showvals,cmap='viridis',ax=ax1,xticklabels=np.around(np.log10(noises), 1), yticklabels=polydegs, vmin = 0.95, vmax = 1.0, fmt = '.3g')
+        print(np.max(TestErrors), np.argmax(TestErrors))
         ax1.set_xlabel(r'$log_{10}(\sigma)$')
         ax1.set_ylabel('Polynomial degree')
         ax1.set_title(r'%s epochs %s Test %s, #datapoints = %i'%(self.epochs,"FFNN",self.cost, int(self.N*self.frac)))
-        h2=sns.heatmap(data=TrainErrors,annot=showvals,cmap='viridis',ax=ax2,xticklabels=np.around(np.log10(noises), 1), yticklabels=polydegs, vmin = 0.95, vmax = 1.0, fmt = '.3g')
-        ax2.set_xlabel(r'$log_{10}(\sigma)$')
-        ax2.set_ylabel('Polynomial degree')
-        ax2.set_title(r'%s epochs %s Train %s'%(self.epochs,"FFNN", self.cost))
+        # h2=sns.heatmap(data=TrainErrors,annot=showvals,cmap='viridis',ax=ax2,xticklabels=np.around(np.log10(noises), 1), yticklabels=polydegs, vmin = 0.95, vmax = 1.0, fmt = '.3g')
+        # ax2.set_xlabel(r'$log_{10}(\sigma)$')
+        # ax2.set_ylabel('Polynomial degree')
+        # ax2.set_title(r'%s epochs %s Train %s'%(self.epochs,"FFNN", self.cost))
         plt.show()
 
     def optparafinder(self, Nloops, noise, epochs, eta_arr, polydeg_arr, nbatch_arr, hlayer_arr, relu_arr, inits):
@@ -146,6 +186,7 @@ class FrankeNN(Project1):
         self.epochs = epochs
         optinds = np.zeros((Nloops, len(labels))).astype(int)
         opterrs = np.zeros(Nloops)
+        bestr2 = 0
         for i in range(Nloops):
             for j,arr in enumerate(arrs):
                 err_arr = np.zeros(len(arr))
@@ -153,12 +194,14 @@ class FrankeNN(Project1):
                     print(i,j,k)
                     self.parachanger(labels[j], val)
                     #err_arr[k] = self.traintesterr(testerr = True)
-                    err_arr[k] = self.kfolderr(ks = np.array([4,5]), method = self.FFNN.fit)
+                    err_arr[k] = self.kfolderr(ks = np.array([2,3,4,5]), method = self.FFNN.fit)
+
                 optind = np.argmax(err_arr)
                 optinds[i,j] = optind
                 self.parachanger(labels[j], arr[optind])
             opterrs[i] = np.max(err_arr)
-        optfin = optinds[-1]
+        optcomb = np.argmax(opterrs)
+        optfin = optinds[optcomb]
         #print("Optimal values:")
         for i,val in enumerate(optfin):
             self.parachanger(labels[i],arrs[i][val])
@@ -183,6 +226,8 @@ class FrankeNN(Project1):
         elif label == "relu":
             self.activation = ReLU(val)
             self.FFNN.activation = ReLU(val)
+        elif label == "epochs":
+            self.epochs = val
 
 def optparaexplorer(Nloops, noise, epochs):
     FNN = FrankeNN()
@@ -192,11 +237,11 @@ def optparaexplorer(Nloops, noise, epochs):
     FNN.initNN()
     FNN.cost = "R2"
 
-    eta_arr = np.array([0.05, 0.1, 0.15, 0.2])#np.array([0.1, 0.15, 0.2])#np.array([0.05, 0.1, 0.15, 0.2])
+    eta_arr = np.array([0.05, 0.1, 0.15, 0.2, 0.3, 0.4])#np.array([0.1, 0.15, 0.2])#np.array([0.05, 0.1, 0.15, 0.2])
     polydeg_arr = np.arange(1,8)#np.arange(1,15)
-    nbatch_arr = np.array([1, 4, 10, 20])#np.array([10, 15])
-    hlayer_arr = [[], [30], [30,15], [60,30,15], [32,16,8]]
-    relu_arr = np.array([0.005, 0.01, 0.015, 0.02])
+    nbatch_arr = np.array([1, 5, 10, 15, 20])#np.array([10, 15])
+    hlayer_arr = [[], [30], [60], [30,15], [60,30,15], [32,16,8], [16,8,4]]
+    relu_arr = np.array([0.005, 0.01, 0.015, 0.02, 0.03])
     inits = [0.15, 5, 10, [30,15], 0.01]
     optinds, opterrs = FNN.optparafinder(Nloops, noise, epochs, eta_arr, polydeg_arr, nbatch_arr, hlayer_arr, relu_arr, inits)
     finopt = optinds[-1]
@@ -208,19 +253,111 @@ def multinoiseoptpara(noises, Nloops, epochs):
         dats.append(optparaexplorer(Nloops, noise, epochs))
     return dats
 
+def optparainstance():
+    Nloops = 3
+    noise = 1e-2
+    epochs = 500
+    noises = np.logspace(-3,-0.5, 11)
+    etas = np.array([0.05, 0.1, 0.15, 0.2, 0.3])
+    from time import time
+    start = time()
+    dat = multinoiseoptpara(noises, Nloops, epochs)
+    end = time()
+    return dat, (end-start)
+
+def convergencefind(etas, noise, polydeg):
+    FNN = FrankeNN()
+    FNN.compnoisy = False
+    FNN.gendat(400, noisefraq = noise, Function = FrankeFunction, deg = (4,4), randpoints = True)
+    FNN.hlayers = [30,15]; FNN.activation = ReLU(0.01); FNN.outactivation = ReLU(1.00)
+    FNN.epochs = 10; FNN.nbatches = 10; FNN.eta = 0.1
+    FNN.initNN()
+    FNN.cost = "MSE"
+    #epoch_arr = np.array([10, 30, 100, 300])
+    epoch_arr = np.logspace(1,4, 7)
+    plt.figure()
+    for eta in etas:
+        print("yo")
+        FNN.eta = eta
+        FNN.parachanger('eta', eta)
+        FNN.multiepoch(polydeg, noise, epoch_arr, newplot = False)
+    plt.xscale("log")
+    plt.legend()
+
+def epochbiasvar():
+    FN1 = FrankeNN()
+    FN1.compnoisy = False
+    FN1.gendat(100, noisefraq=1e-1, Function = FrankeFunction, deg =(4,4), randpoints = True)
+    FN1.hlayers = [60,30,15]; FN1.activation = ReLU(0.005); FN1.outactivation = ReLU(1.00)
+    FN1.epochs = 500; FN1.nbatches = 15; FN1.eta = 0.2
+    FN1.initNN()
+    paramname = "epochs"; param_arr = np.logspace(1,3,11).astype(int)#np.array([10,30,100,300,1000])
+    #paramname = "hlayer"; param_arr = [[64], [64, 32], [64,32,16], [64,32,16,8], [64,32,16,8,4]]
+    K = 20
+
+    FN1.biasvar(K, param_arr, paramname)
+
+def etabiasvar():
+    FN1 = FrankeNN()
+    FN1.compnoisy = False
+    FN1.gendat(100, noisefraq=1e-1, Function = FrankeFunction, deg =(4,4), randpoints = True)
+    FN1.hlayers = [60,30,15]; FN1.activation = ReLU(0.005); FN1.outactivation = ReLU(1.00)
+    FN1.epochs = 500; FN1.nbatches = 15; FN1.eta = 0.2
+    FN1.initNN()
+    paramname = "eta"; param_arr = np.logspace(-2,0.5, 10)
+    K = 20
+
+    FN1.biasvar(K, param_arr, paramname)
+
+def hlayerbiasvar():
+    FN1 = FrankeNN()
+    FN1.compnoisy = False
+    FN1.gendat(400, noisefraq=1e-2, Function = FrankeFunction, deg =(4,4), randpoints = True)
+    FN1.hlayers = [60,30,15]; FN1.activation = ReLU(0.005); FN1.outactivation = ReLU(1.00)
+    FN1.epochs = 500; FN1.nbatches = 15; FN1.eta = 0.2
+    FN1.initNN()
+    #paramname = "epochs"; param_arr = np.array([10,100,1000])
+    paramname = "hlayer"; param_arr = [[64], [64, 32], [64,32,16], [64,32,16,8], [64,32,16,8,4]]
+    K = 15
+    FN1.biasvar(K, param_arr, paramname)
+
+def batchbiasvar():
+    FN1 = FrankeNN()
+    FN1.compnoisy = False
+    FN1.gendat(100, noisefraq=1e-1, Function = FrankeFunction, deg =(4,4), randpoints = True)
+    FN1.hlayers = [60,30,15]; FN1.activation = ReLU(0.005); FN1.outactivation = ReLU(1.00)
+    FN1.epochs = 500; FN1.nbatches = 15; FN1.eta = 0.2
+    FN1.initNN()
+    #paramname = "epochs"; param_arr = np.array([10,100,1000])
+    paramname = "nbatch"; param_arr = np.array([1, 5, 10, 20, 40, 100])
+    K = 15
+    FN1.biasvar(K, param_arr, paramname)
+
+def degvnoisevhlayererr():
+    FN1 = FrankeNN()
+    FN1.compnoisy = False
+    FN1.gendat(400, noisefraq=1e-1, Function = FrankeFunction, deg =(4,4), randpoints = True)
+    FN1.hlayers = [60,30,15]; FN1.activation = ReLU(0.005); FN1.outactivation = ReLU(1.00)
+    FN1.epochs = 500; FN1.nbatches = 15; FN1.eta = 0.2
+    FN1.cost = "R2"
+    FN1.initNN()
+    degs = np.arange(1,9)
+    for hlayers in [[], [30], [30,15], [60,30,15]]:
+        FN1.parachanger('hlayer', hlayers)
+        FN1.degvnoiseerr(degs, np.logspace(-3, -0.5, 11))
+
 if __name__ == "__main__":
     pass
-    Nloops = 5
-    noise = 3e-3
-    epochs = 300
-    noises = np.logspace(-2.5,-0.5, 5)
-    dat = multinoiseoptpara(noises, Nloops, epochs)
-    #FNN, optinds, opterrs = optparaexplorer(Nloops, noise, epochs)
-    # FN1 = FrankeNN()
-    # FN1.compnoisy = False
-    # FN1.gendat(400, noisefraq=1e-2, Function = FrankeFunction, deg =(6,6), randpoints = True)
-    # FN1.hlayers = [60,30,15]; FN1.activation = ReLU(0.005); FN1.outactivation = ReLU(1.00); FN1.epochs = 10; FN1.nbatches = 15; FN1.eta = 0.2
-    # FN1.initNN()
+    #dat, t_elapsed = optparainstance()  #WARNING: will take 5 hours-1 day depending on hardware.
+
+    FN1 = FrankeNN()
+    FN1.compnoisy = False
+    FN1.gendat(400, noisefraq=1e-2, Function = FrankeFunction, deg =(4,4), randpoints = True)
+    FN1.hlayers = [60,30,15]; FN1.activation = ReLU(0.005); FN1.outactivation = ReLU(1.00)
+    FN1.epochs = 500; FN1.nbatches = 15; FN1.eta = 0.2
+    FN1.cost = "R2"
+    FN1.initNN()
+    #FN1.biasvar(10, np.arange(1,12))
     # FN1.cost = "R2"
     # #degs = np.arange(1,6)
     # #noises = np.logspace(-2.5,-0.5,5)
