4f056ba673d450b48ae76ba56f053a20e7af2d9a
Magnus-SI <40592328+Magnus-SI@users.noreply.github.com>
2019-12-09 13:06:38 +0100

    some additions

--- Project3/models.py
+++ Project3/models.py
@@ -5,10 +5,87 @@ from sklearn import metrics
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+class NNxgb:
+    def __init__(self, num_round_xgb):
+        self.paramxgb = {'max_depth': 3,
+                      'eta': 1,
+                      'objective': 'binary:logistic',
+                      'nthread': 4,
+                      'eval_metric': 'auc',
+                      'booster': 'dart'}
+
+        self.paramNN = {
+                      'layers': [128,2],
+                      'activations': ['relu', 'softmax'],
+                      'optimizer': 'adam',
+                      'loss': 'categorical_crossentropy',
+                      'metrics': ['AUC'],
+                      'epochs': 20,
+                      'batch_size': 32
+                     }
+        self.num_round_xgb = num_round_xgb
+        self.initNN()
+
+    def initNN(self):
+        model = tf.keras.models.Sequential()
+        model.add(tf.keras.layers.Dense(128, activation = 'relu',
+                                        input_shape = (8,), name = 'layer1'))
+        model.add(tf.keras.layers.Dense(2, activation = 'softmax'))
+        # model = tf.keras.models.Sequential([
+        #         tf.keras.layers.Dense(int(i), activation = j, name = "%ilayer"%i)\
+        #         for i,j in zip(self.paramNN['layers'], self.paramNN['activations'])
+        #     ])
+
+        model.compile(
+            optimizer = self.paramNN['optimizer'],
+            loss = self.paramNN['loss'],
+            metrics = self.paramNN['metrics']
+        )
+        self.NN = model
+        self.interm = tf.keras.models.Model(inputs = model.input,
+                                            outputs = model.get_layer('layer1').output)
+
+    def paramchanger(self, label, value, paramtype = 'xgb'):
+        if paramtype == 'xgb':
+            self.paramxgb[label] = value
+        else:
+            self.paramCNN[label] = value
+            if label not in ['epochs', 'batch_size']:
+                self.initmodel()
+
+    def expanddim(self,y):
+        ynew = np.zeros((len(y), 2))
+        ynew[:, 0] = y
+        ynew[:, 1] = 1-y
+        return ynew
+
+    def fitNN(self, X, y):
+        if len(y.shape)==1:
+            y = self.expanddim(y)
+        self.NN.fit(X, y,
+                      epochs = self.paramNN['epochs'],
+                      batch_size = self.paramNN['batch_size'],
+                      verbose = True
+                      )
+
+    def fitxgb(self, X, y):
+        X = self.interm.predict(X)
+        dfit = xgb.DMatrix(X, label = y)
+        self.bst = xgb.train(self.paramxgb, dfit, self.num_round_xgb)
+
+    def fit(self, X,y):
+        self.fitNN(X,y)
+        self.fitxgb(X,y)
+
+    def predict(self, X):
+        X = self.interm.predict(X)
+        dpred = xgb.DMatrix(X)
+        return self.bst.predict(dpred)
+
 class XGBoost:     #may want this for general use, not yet used
     def __init__(self,num_round):
         self.param = {'max_depth': 3, 'eta': 1, 'objective': 'binary:logistic',
-                      'nthread': 8, 'eval_metric': 'auc'}
+                      'nthread': 8, 'eval_metric': 'auc', 'booster': 'dart'}
         self.num_round = num_round
 
     def paramchanger(self, label, value):
@@ -78,7 +155,7 @@ class analyze:
         self.models = models
         self.df, self.xlabels, self.ylabels = loader()
         self.N = len(self.df)
-        self.traintestsimple(train_frac = 0.8)
+        self.traintestsimple(train_frac = 0.5)
 
     def traintestsplit(self, traininds, testinds):
         self.dftrain = self.df.iloc[traininds]
@@ -121,9 +198,15 @@ class analyze:
 if __name__ == "__main__":
     from pulsar import pulsardat
     loader = pulsardat()
-    model1 = XGBoost(num_round = 100)
+    model1 = XGBoost(num_round = 10)
+    model4 = XGBoost(num_round = 10)
+    #model4.paramchanger('objective', 'reg:squarederror')
+    model4.paramchanger('booster', 'gblinear')
+    model5 = XGBoost(num_round = 10)
+    model5.paramchanger('booster', 'gbtree')
     model2 = NNmodel()
-    models = [model1, model2]
+    model3 = NNxgb(10)
+    models = [model1, model2, model3, model4, model5]
     A = analyze(models, loader)
     A.traintestpred("ok")
     A.plot_PR()
